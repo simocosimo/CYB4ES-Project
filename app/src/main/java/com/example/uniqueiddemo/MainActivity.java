e3eb2c7322f1b28925b5f37d56ffa6ab102e264c
Davide Innocenzi <s305388@studenti.polito.it>
2023-05-25 16:35:37 +0200

    Creata app basilare

---
+++ app/src/main/java/com/example/uniqueiddemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.uniqueiddemo;
+
+import static com.google.android.material.R.color.design_default_color_primary_variant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import com.example.uniqueiddemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new AuthenticationFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.authmenu){
+                replaceFragment(new AuthenticationFragment());
+                return true;
+            } else if (item.getItemId() == R.id.verifmenu) {
+                replaceFragment(new VerificationFragment());
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
